3a1c68914bf9414083bab6431d19db28dd901767
Avashbhandari01 <avash115@gmail.com>
2024-11-09 12:54:16 +0545

    DbContext and generic repo.

---
+++ JobCandidateHub.Infrastructure/Implementation/Repositories/GenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using JobCandidateHub.Application.Interfaces.Repositories;
+using JobCandidateHub.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Linq;
+
+namespace JobCandidateHub.Infrastructure.Implementation.Repositories
+{
+    public class GenericRepository(ApplicationDbContext dbContext) : IGenericRepository
+    {
+        public async Task<IEnumerable<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>>? filter = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, string includeProperties = "") where TEntity : class
+        {
+            IQueryable<TEntity> query = dbContext.Set<TEntity>();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            query = includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+            if (orderBy != null)
+            {
+                return await orderBy(query).ToListAsync();
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<TEntity?> GetFirstOrDefaultAsync<TEntity>(Expression<Func<TEntity, bool>> filter) where TEntity : class
+        {
+            return await dbContext.Set<TEntity>().FirstOrDefaultAsync(filter);
+        }
+
+        public async Task<int> InsertAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+
+            await dbContext.Set<TEntity>().AddAsync(entity);
+
+            await dbContext.SaveChangesAsync();
+
+            var ret = 0;
+
+            var key = typeof(TEntity).GetProperties().FirstOrDefault(p =>
+                p.CustomAttributes.Any(attr =>
+                    attr.AttributeType == typeof(KeyAttribute)));
+
+            if (key != null)
+            {
+                ret = (int)(key.GetValue(entity, null) ?? 0);
+            }
+
+            return ret;
+        }
+
+        public async Task UpdateAsync<TEntity>(TEntity entityToUpdate) where TEntity : class
+        {
+            ArgumentNullException.ThrowIfNull(entityToUpdate);
+
+            dbContext.Update(entityToUpdate);
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ JobCandidateHub.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JobCandidateHub.Domain.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobCandidateHub.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
