56dcdb1f8891a28b164257f64646bebac96dd832
Avashbhandari01 <avash115@gmail.com>
2024-11-09 13:27:35 +0545

    Add infrastructure service.

---
+++ JobCandidateHub.Infrastructure/Dependency/InfrastructureServices.cs
@@ -0,0 +1,28 @@
+﻿using JobCandidateHub.Application.Interfaces.Repositories;
+using JobCandidateHub.Application.Interfaces.Services;
+using JobCandidateHub.Infrastructure.Implementation.Repositories;
+using JobCandidateHub.Infrastructure.Implementation.Services;
+using JobCandidateHub.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobCandidateHub.Infrastructure.Dependency
+{
+    public static class InfrastructureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString,
+                    b => b.MigrationsAssembly("Infrastructure")));
+
+            services.AddTransient<IGenericRepository, GenericRepository>();
+            services.AddScoped<ICandidateService, CandidateService>();
+
+            return services;
+        }
+    }
+}
--- JobCandidateHub/Program.cs
+++ JobCandidateHub/Program.cs
@@ -1,20 +1,35 @@
+using JobCandidateHub.Infrastructure.Dependency;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+
+builder.Services.AddInfrastructureServices(configuration);
+
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(policyBuilder =>
+{
+    policyBuilder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader().SetIsOriginAllowed((host) => true);
+});
 
 app.UseHttpsRedirection();
 
